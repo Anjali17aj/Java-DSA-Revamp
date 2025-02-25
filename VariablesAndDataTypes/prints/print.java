b1cbfb3e827e2d62fc4bb1cc16d96930af28c65b
unknown <sayhellotoanjali@gmail.com>
2025-02-25 17:51:03 +0530

    Variables and Data Types

---
+++ VariablesAndDataTypes/prints/print.java
@@ -0,0 +1,7 @@
+public class print{
+    public static void main(String args[]){
+        System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("hello \n bye");
+    }
+}
