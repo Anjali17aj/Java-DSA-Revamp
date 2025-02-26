ccf6ab179bc39c7a3a723760272e3fc3a264a15a
unknown <sayhellotoanjali@gmail.com>
2025-02-26 10:27:58 +0530

    Variables and data types

---
+++ VariablesAndDataTypes/variables/variables.java
@@ -0,0 +1,8 @@
+public class variables{
+    public static void main(String args[]){
+        int a=17;
+        int b=18;
+        System.out.println(a);
+        System.out.println("b");
+    }
+}
