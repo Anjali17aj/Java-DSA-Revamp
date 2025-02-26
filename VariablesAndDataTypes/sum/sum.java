b41f9d2cfac0caeb8c9396a2880324b4fb9c1e12
unknown <sayhellotoanjali@gmail.com>
2025-02-26 12:02:39 +0530

    Variables and data types

---
+++ VariablesAndDataTypes/sum/sum.java
@@ -0,0 +1,14 @@
+public class sum {
+    public static void main(String args[]){
+        //code to calculate sum
+
+        /*
+         * code to calculate sum
+         * of two numbers
+         */
+        int a=5;
+        int b=6;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
