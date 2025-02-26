b23df8fcce80c2e9c63242f6c3395517909cb1e1
unknown <sayhellotoanjali@gmail.com>
2025-02-26 11:41:26 +0530

    Variables and data types

---
+++ VariablesAndDataTypes/datatypes/datatypes.java
@@ -0,0 +1,15 @@
+public class datatypes {
+    public static void main(String args[]){
+    byte b = 8;
+    System.out.println(b);
+    char ch = 'a';
+    System.out.println(ch);
+    boolean var = false;
+    //oat price = 10.5;
+    int num = 10;
+    //long
+    //double
+    short n = 240;
+    System.out.println(n);
+    }
+}
---
+++ VariablesAndDataTypes/vars/variablee.java
@@ -0,0 +1,11 @@
+public class variablee {
+    public static void main(String args[]){
+        int a=5;
+        String name = "Tony stark";
+        System.out.println(name);
+
+        a=6;
+        System.out.println(a);
+    }
+}
+
