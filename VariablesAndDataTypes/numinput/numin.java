5d6890d1f2296738c302522d48a639b1c1795d6a
unknown <sayhellotoanjali@gmail.com>
2025-02-26 16:09:17 +0530

    Variables and Data types

--- VariablesAndDataTypes/numinput/numin.java
+++ VariablesAndDataTypes/numinput/numin.java
@@ -1,9 +1,7 @@
 import java.util.*;
-public class inputss {
+public class numin {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
 int num = sc.nextInt();
 System.out.println(num);
     }
